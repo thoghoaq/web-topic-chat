4e0edd545f65c7677ff8dde8dd9d0b5952dcc3bd
ThongHT <thonght150201@gmail.com>
2023-05-15 09:44:37 +0700

    api register

--- Infra/WebTopicChat.Infrastructure/Services/Auth/IAuthService.cs
+++ Infra/WebTopicChat.Infrastructure/Services/Auth/IAuthService.cs
@@ -1,4 +1,5 @@
-﻿using WebTopicChat.Domain.DTOs.View.Auth;
+﻿using WebTopicChat.Domain.DTOs.Request.Auth;
+using WebTopicChat.Domain.DTOs.View.Auth;
 using WebTopicChat.Domain.DTOs.View.Common;
 
 namespace WebTopicChat.Infrastructure.Services.Auth
@@ -6,5 +7,6 @@ namespace WebTopicChat.Infrastructure.Services.Auth
     public interface IAuthService
     {
         KeyValuePair<MessageViewModel ,LoginViewModel?> Login(string userName, string password);
+        LoginViewModel Register(RegisterRequestModel model);
     }
 }
