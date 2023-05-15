3a5bd816cd14eb9e10a1fad37b297afc56e224c2
trangnguyen <trangnguyen2812000@gmail.com>
2023-05-15 17:29:08 +0700

    context

--- Core/WebTopicChat.Application/Context/TopicChatContext.cs
+++ Core/WebTopicChat.Application/Context/TopicChatContext.cs
@@ -25,7 +25,7 @@ namespace WebTopicChat.Application.Context
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
 
-                optionsBuilder.UseSqlServer("Server = CL-5CD723106P; Database = TopicChat; Trusted_Connection = True; Encrypt = False");
+                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=TopicChat(new);Integrated Security=True");
 
             }
         }
